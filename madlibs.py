3f4bc6ba90c8e8a7a0642a9bacadfb78f27eceea
Sidnei <sidnei.gordo@ciit.edu.ph>
2026-02-23 20:26:53 +0800

    MADLIBS ACT
    Madlib activity

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter another adjective: ")
+verb1 = input("Enter a verb (action word): ")
+adjective3 = input("Enter one last adjective: ")
+verb2 = input ("Enter another verb:")
+
+print("\n--- YOUR STORY ---")
+print(f"Today I went to a {adjective1} river.")
+print(f"In the river, I saw a {noun1}.")
+print(f"The {noun1} was {adjective2} and started to {verb1} at me.")
+print(f"I was {adjective3} so I {verb2} away!")
